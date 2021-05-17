da8859179f9df66c9909d9c5e6708bf1371a048d
Tariq <selawi.be@gmail.com>
2021-05-17 15:57:30 +0200

    update

--- wordpress/wp-content/themes/cms-restaurant/front-page.php
+++ wordpress/wp-content/themes/cms-restaurant/front-page.php
@@ -1,23 +1,26 @@
-<?php get_header(); ?>
 <?php
+get_header();
+$text = get_field('home_text');
 $image = get_field('feature_image');
-$pictureSize = $image['sizes']['my_custom_size'];
+$picture = $image['sizes']['my_custom_size'];
 ?>
        
 
-
-       <img src="<?= $pictureSize ?>" class="img-fluid">
+<div class="banner-home"> 
+<img src="<?php echo $picture; ?>" class="bg-img">      
 <div class="container">
-
     <?php if (have_posts()):
         while (have_posts()):
             the_post(); ?>
-
-            <div><?php the_content(); ?></div>
-            
+            <div><?php the_content(); ?>
+            <?php if ($text): ?>
+            <h1 class="title"><?= $text ?></h1>
+            <?php endif; ?>
+        </div>   
     <?php
         endwhile;
     endif; ?>
+    
+</div> 
 </div>
-
 <?php get_footer(); ?>
